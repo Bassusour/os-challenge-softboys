4529ab2a87b72a2ae997799a1dc0489e6a4b28c3
Chermas <victor@miwire.net>
2022-09-15 10:52:28 +0200

    TCP Connection setup

--- server.c
+++ server.c
@@ -1,73 +1,73 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-
 #include <netdb.h>
 #include <netinet/in.h>
-
+#include <stdlib.h>
 #include <string.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#define MAX 80
+#define SA struct sockaddr
+   
 
+   
+// Driver function
+int main(int argc, char **argv)
+{
+    int sockfd, connfd, len, portno;
+    struct sockaddr_in servaddr, cli;
 
-int main(int argc, char **argv){
-    int sockfd, newsockfd, portno, clilen;
-    char buffer[256];
-    struct sockaddr_in serv_addr, cli_addr;
-    int  n;
-
-/*
     if (argc <= 1) {
         perror("ERROR give port");
         exit(1);  
     }
-    */
-
+   
+    // socket create and verification
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
-
-    if (sockfd < 0) {
-      perror("ERROR opening socket");
-      exit(1);
+    if (sockfd == -1) {
+        printf("socket creation failed...\n");
+        exit(0);
     }
+    else
+        printf("Socket successfully created..\n");
+    bzero(&servaddr, sizeof(servaddr));
 
-    bzero((char *) &serv_addr, sizeof(serv_addr));
-    //portno = atoi(argv[1]);
-
-    portno = 5001;
-
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_addr.s_addr = INADDR_ANY;
-    serv_addr.sin_port = htons(portno);
-
-    if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
-      perror("ERROR on binding");
-      exit(1);
-    }
-
-    listen(sockfd,5);
-    clilen = sizeof(cli_addr);
-
-    newsockfd = accept(sockfd, (struct sockaddr *)&cli_addr, &clilen);
-
-    if (newsockfd < 0) {
-      perror("ERROR on accept");
-      exit(1);
-    }
-
-    bzero(buffer,256);
-    n = read( newsockfd,buffer,255 );
+    portno = atoi(argv[1]);
    
-    if (n < 0) {
-      perror("ERROR reading from socket");
-      exit(1);
+    // assign IP, PORT
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    servaddr.sin_port = htons(portno);
+   
+    // Binding newly created socket to given IP and verification
+    if ((bind(sockfd, (SA*)&servaddr, sizeof(servaddr))) != 0) {
+        printf("socket bind failed...\n");
+        exit(0);
     }
+    else
+        printf("Socket successfully binded..\n");
    
-    printf("Here is the message: %s\n",buffer);
-
-   /* Write a response to the client */
-    n = write(newsockfd,"I got your message",18);
+    // Now server is ready to listen and verification
+    if ((listen(sockfd, 5)) != 0) {
+        printf("Listen failed...\n");
+        exit(0);
+    }
+    else
+        printf("Server listening..\n");
+        printf("%d\n", servaddr.sin_addr.s_addr);
+    len = sizeof(cli);
    
-   if (n < 0) {
-      perror("ERROR writing to socket");
-      exit(1);
-   }
-    return 0;
+    // Accept the data packet from client and verification
+    connfd = accept(sockfd, (SA*)&cli, &len);
+    if (connfd < 0) {
+        printf("server accept failed...\n");
+        exit(0);
+    }
+    else
+        printf("server accept the client...\n");
+   
+   
+    // After chatting close the socket
+    close(sockfd);
 }
