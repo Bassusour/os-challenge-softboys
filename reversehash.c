1d06b29cba5f520779eaea45bdbbdc91c6d6f3da
Theodor Gaard Grønlund <s183326@student.dtu.dk>
2022-09-22 10:49:47 +0200

    Updated reversehash function

--- reversehash.c
+++ reversehash.c
@@ -2,18 +2,26 @@
 #include <stdint.h>
 
 uint64_t reversehash(uint64_t start, uint64_t end, uint8_t hash[32]){
-    for(uint64_t i = start; i <= end; i++){
-        uint8_t buffer[32];
+    for(uint64_t num = start; num <= end; num++){
 
+        unsigned char ibuf[sizeof(num)];
+        memcpy(ibuf, &num, sizeof(num));
+        
+        unsigned char obuf[32];
 
+        //printf("%d \n",strlen(ibuf) );
+        SHA256(ibuf, sizeof(ibuf), obuf);
 
+        // int i;
+        // for (i = 0; i < 32; i++) {
+        //     printf("%02x ", obuf[i]);
+        // }
+        // printf("\n");
 
-        if(!strcmp(buffer,hash)){
-            return i;
+        if(!strcmp(obuf,hash)){
+            return num;
         }
     }
-
-
 
     return -1;
 }
